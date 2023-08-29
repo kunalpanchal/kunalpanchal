471a77a68b49abab7d2e8c47200ad05e681e8571
Kunal Panchal <hello@kunalpanchal.in>
2023-08-29 21:21:19 +0200

    chore: move content to seperate file

---
+++ content.mjs
@@ -0,0 +1,63 @@
+const openSourceProjects = [
+    { link: "https://github.com/kunalpanchal/secure-env", title: "secure-env", explanation: "NPM that encrypts and protects your process.env" },
+    { link: "https://github.com/kunalpanchal/just", title: "just", explanation: "A lazy wrapper for npm/yarn/pnpm" },
+    { link: "https://github.com/kunalpanchal/mt940-viewer", title: "mt940-viewer", explanation: "A mt940-viewer formatted transactions viewer" },
+    { link: "https://github.com/upgrad/react-image-gallery", title: "react-s3-image-gallery", explanation: "A react gallery component with support for uploading images to S3" },
+    { link: "https://github.com/kunalpanchal/relevant-bookmarks", title: "relevant-bookmarks", explanation: "A Chrome extension that gives you a list of relevant bookmarks specific to the URL you are in." },
+    { link: "https://github.com/shaaijs", title: "Shaai.js", explanation: "A pluggable blogging framework" },
+    { link: "https://github.com/play-this/play-this", title: "Play This", explanation: "A PWA that lets you create a club and let people control your playlist." },
+    { title: "Lanuks", explanation: "A customized linux distro for the PWAs of tomorrow (WIP)" }
+];
+
+/*
+ <!--                     <li><a href="https://outgrow.co">Outgrow.co</a> - A New Marketing Toolkit. Interactive Calculators
+                        and Quizzes.</li>
+                    <li><a href="https://upgrad.com">upGrad.com</a> - Working on the marketing platform.</li> -->
+                    <!--                     <li><span>Onob</span> - A android app controlled IoT based knob to control your stove.</li> -->
+                    <!--                     <li><span>Terribly Tiny Tales Video Maker</span> - A convestation chat maker for TTT.</li> -->
+*/
+const projects = [
+    { link: "https://sitaby.netlify.app", title: "SitAby", description: "A IoT based posture proctor. <span style=\"color:#cc0000\">Patent: 3648/DEL/2015</span>. Infymakers Award winner India." },
+    { link: "https://terriblytinytales.com/app", title: "Terribly Tiny Tales Social Network", description: "A full-fleged social network for writers.", playstoreLink: "https://play.google.com/store/apps/details?id=com.terriblytinytales.ttt", appStoreLink: "https://itunes.apple.com/in/app/terribly-tiny-tales/id1227454777?ls=1" },
+    { link: "https://play.google.com/store/apps/details?id=com.kay.june.disguisedfilehider", title: "Disguised Encrypter", description: "AES256 files encrypter for android, disguised as a calculator.", playstoreLink: "https://play.google.com/store/apps/details?id=com.kay.june.disguisedfilehider" },
+    { link: "https://play.google.com/store/apps/details?id=gptw.cultureinshorts.in", title: "Culture Inshorts", description: "is a series of short articles reader app", playstoreLink: "https://play.google.com/store/apps/details?id=gptw.cultureinshorts.in", appStoreLink: "https://itunes.apple.com/in/app/culture-inshorts/id1419586346" },
+    { link: "https://play.google.com/store/apps/details?id=com.indiadesignid.app", title: "India Design ID 2019", description: "Country&rsquo;s best luxury design week, event application.", playstoreLink: "https://play.google.com/store/apps/details?id=com.indiadesignid.app", appStoreLink: "https://itunes.apple.com/in/app/india-design-id-2019/id1450007977" },
+    { link: "https://play.google.com/store/apps/details?id=com.tictactoe.tictoe", title: "Tic Toe", description: "Not a usual Tic Tac Toe game.", playstoreLink: "https://play.google.com/store/apps/details?id=com.tictactoe.tictoe", appStoreLink: "https://apps.apple.com/us/app/tic-toe/id1474668637" },
+];
+
+const workTimeline = [
+    { link: "https://www.spotify.com/", company: "Spotify", timeline: "October 2022 - Present" },
+    { link: "https://www.deliveryhero.com/", company: "Delivery Hero", timeline: "February 2020 - September 2022" },
+    { link: "https://www.upgrad.com/", company: "upGrad.com", timeline: "March 2019 - February 2020" },
+    { link: "https://www.terriblytinytales.com/", company: "TerriblyTinyTales.com", timeline: "December 2017 - March 2019" },
+    { link: "https://www.outgrow.co/", company: "Outgrow.co", timeline: "August 2016 - December 2017" },
+];
+
+const blogs = [
+    { link: "https://dev.to/kay_2695px/react-india-2019-1goa", title: "React India 2019 Summarized" },
+    {
+        link: "https://codeburst.io/does-any-harm-typescript-c811ad3b00e8", title: "Does typescript any harm?"
+    },
+    { link: "https://medium.com/hackernoon/dope-vs-code-extensions-for-a-javascript-developer-7981625f4c4a", title: "VS code extensions for a Javascript Developer" },
+    { link: "https://codeburst.io/how-secure-is-your-environment-file-in-node-js-7c4d2ed0d15a", title: "How secure is your environment file in Node.js" },
+]
+
+const links = [
+    { link: 'http://github.kunalpanchal.in', title: 'GitHub' },
+    { link: 'http://twitter.kunalpanchal.in', title: 'Twitter (X)' },
+    { link: 'http://l.kunalpanchal.in', title: 'Linkedin' },
+    { link: 'http://dev.kunalpanchal.in', title: 'Dev.to' },
+    { link: 'http://so.kunalpanchal.in', title: 'Stack Overflow' },
+];
+
+const extendedLinks = [
+    { link: 'https://unsplash.com/@panchalkunal', title: 'Unsplash' },
+    { link: 'https://www.instagram.com/the_hysteresis', title: 'Instagram' },
+    { link: 'https://www.reddit.com/user/panchalkunal', title: 'Reddit' },
+    { link: 'http://medium.com/@kunalpanchal', title: 'Medium' },
+    { link: 'https://www.strava.com/athletes/48643619', title: 'Starva' },
+    { link: 'https://www.youtube.com/c/KunalPanchal2695', title: 'YouTube Channel' },
+    { link: 'https://open.spotify.com/user/fhkgexqcgefm05zipq1fa1fh0', title: 'Spotify' },
+];
+
+export { openSourceProjects, workTimeline, blogs, projects, links, extendedLinks }
